4a111f877b4e8ed595fb1192952ffb4b38c26ddb
juancruzgon05 <162723271+juancruzgon05@users.noreply.github.com>
2024-05-31 11:31:12 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,48 +1,38 @@
-
 def create_inventory(items):
-    """Create a dict that tracks the amount (count) of each element on the `items` list.
+	dic=dict()
+	for i in items:
+		dic[i]= items.count(i)
+	return dic
 
-    :param items: list - list of items to create an inventory from.
-    :return: dict - the inventory dictionary.
-    """
-    return {}
 
 
 def add_items(inventory, items):
-    """Add or increment items in inventory using elements from the items `list`.
-
-    :param inventory: dict - dictionary of existing inventory.
-    :param items: list - list of items to update the inventory with.
-    :return: dict - the inventory updated with the new items.
-    """
-    return {}
-
+	for i in items:
+		if i in inventory:
+			inventory[i]+=1
+		else:
+			inventory[i]=1
+	return inventory
 
 def decrement_items(inventory, items):
-    """Decrement items in inventory using elements from the `items` list.
-
-    :param inventory: dict - inventory dictionary.
-    :param items: list - list of items to decrement from the inventory.
-    :return: dict - updated inventory with items decremented.
-    """
-    return {}
-
+	for i in items:
+		if inventory[i]>0:
+			inventory[i]-=1
+		else:
+			inventory[i]=0
 
 def remove_item(inventory, item):
-    """Remove item from inventory if it matches `item` string.
-
-    :param inventory: dict - inventory dictionary.
-    :param item: str - item to remove from the inventory.
-    :return: dict - updated inventory with item removed. Current inventory if item does not match.
-    """
-    return {}
-
+	if item in inventory:
+		del inventory[item]
+	return inventory
 
 def list_inventory(inventory):
-    """Create a list containing all (item_name, item_count) pairs in inventory.
+	x=[]
+	for ele in inventory.items():
+		if ele[1]!=0:
+			x += [ele]
+	return x
 
-    :param inventory: dict - an inventory dictionary.
-    :return: list of tuples - list of key, value pairs from the inventory dictionary.
-    """
-    return {}
+
+
 
